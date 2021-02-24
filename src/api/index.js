858d499e260c008b06c9265b978de84777fc1296
Manav Misra <manavm@visionwebsoft.com>
2021-02-23 18:28:26 -0600

    :construction: Update products from api

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,10 @@ const api = {
   async index(
     endpoint = "http://my-json-server.typicode.com/claim-academy-js/products/products"
   ) {
+    // res is a RESPONSE STREAM
     const res = await fetch(endpoint);
+
+    // Read the STREAM BODY as JSON
     return res.json();
   },
 };
--- src/components/FilterableProductTable/ProductTable/index.js
+++ src/components/FilterableProductTable/ProductTable/index.js
@@ -1,3 +1,18 @@
-const ProductTable = () => <table></table>;
+import api from "api";
+import { useEffect, useState } from "react";
+
+const ProductTable = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      // Await results of reading stream as JSON
+      const productData = await api.index();
+      setProducts(() => productData);
+    })();
+  });
+
+  return <table></table>;
+};
 
 export default ProductTable;
