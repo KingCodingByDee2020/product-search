3d54e365abdedf4791a4a2b453361bd5a15e52a4
Manav Misra <manavm@visionwebsoft.com>
2021-02-24 16:04:10 -0600

    Add and style layout for Search Bar with checkbox

--- src/components/FilterableProductTable/SearchBar/index.js
+++ src/components/FilterableProductTable/SearchBar/index.js
@@ -1,10 +1,17 @@
 const SearchBar = () => (
-  <div className="mb-4">
-    <label htmlFor="search" className="sr-only">
-      Search
-    </label>
-    <input type="search" id="search" placeholder="Search..." />
-  </div>
+  <>
+    <div className="mb-4">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input type="search" id="search" placeholder="Search..." />
+    </div>
+
+    <div className="flex items-center mb-4">
+      <input type="checkbox" id="in-stock-only" className="mr-2" />
+      <label htmlFor="in-stock-only">Only show products in stock</label>
+    </div>
+  </>
 );
 
 export default SearchBar;
