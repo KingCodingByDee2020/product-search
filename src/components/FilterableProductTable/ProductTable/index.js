165964b64810a62898d4088fa85a5f043943aa10
Manav Misra <manavm@visionwebsoft.com>
2021-02-25 09:01:27 -0600

    :bug: Add empty dependency array

--- src/components/FilterableProductTable/ProductTable/index.js
+++ src/components/FilterableProductTable/ProductTable/index.js
@@ -1,35 +1,32 @@
 import api from "api";
 import { useEffect, useState } from "react";
-
 import TBody from "./TBody";
 
 /**
-  * TODO: ProductTable receives filter props from FilterableProductTAble
-  * TODO: PRoductTable applies the filters and seends filtered products to TBody
+ * TODO: ProductTable receives filter props from FilterableProductTAble
+ * TODO: PRoductTable applies the filters and seends filtered products to TBody
  */
 
 const ProductTable = () => {
   const [products, setProducts] = useState([]);
 
-  useEffect(
-    () => {
-      (async () => {
-        // Await results of reading stream as JSON
-        const productData = await api.index();
-        setProducts(() => productData);
-      })();
-    },
-  );
+  useEffect(() => {
+    (async () => {
+      // Await results of reading stream as JSON
+      const productData = await api.index();
+      setProducts(() => productData);
+    })();
+  }, []);
 
   return (
-    <table className="w-1/4">
+    <table>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Price</th>
+          <th className="text-left">Name</th>
+          <th className="text-left">Price</th>
         </tr>
       </thead>
-      <TBody products={products}/>
+      <TBody products={products} />
     </table>
   );
 };
