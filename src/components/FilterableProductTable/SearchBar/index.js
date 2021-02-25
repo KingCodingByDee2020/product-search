41e59b7b30ad0316008400093efd9c907f4a9ce1
Manav Misra <manavm@visionwebsoft.com>
2021-02-25 16:51:53 -0600

    :construction: Update state from controlled Search

--- src/components/FilterableProductTable/SearchBar/index.js
+++ src/components/FilterableProductTable/SearchBar/index.js
@@ -1,17 +1,34 @@
-const SearchBar = () => (
+import PropTypes from "prop-types";
+
+const SearchBar = ({ searchHandler, inStockHandler }) => (
   <>
     <div className="mb-4">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
-      <input type="search" id="search" placeholder="Search..." />
+      <input
+        type="search"
+        id="search"
+        placeholder="Search..."
+        onKeyUp={searchHandler}
+      />
     </div>
 
     <div className="flex items-center mb-4">
-      <input type="checkbox" id="in-stock-only" className="mr-2" />
+      <input
+        type="checkbox"
+        id="in-stock-only"
+        className="mr-2"
+        onChange={inStockHandler}
+      />
       <label htmlFor="in-stock-only">Only show products in stock</label>
     </div>
   </>
 );
+
+SearchBar.propTypes = {
+  searchHandler: PropTypes.func.isRequired,
+  inStockHandler: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
--- src/components/FilterableProductTable/index.js
+++ src/components/FilterableProductTable/index.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Table from "./ProductTable";
 import Search from "./SearchBar";
 
-const FilterableProductTable = () => (
-  <main className="container mx-auto w-1/2 flex flex-col items-center mt-8">
-    <Search />
-    <Table />
-  </main>
-);
+const FilterableProductTable = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  function handleKeyUp({ target: { value } }) {
+    setSearchTerm(() => value);
+  }
+
+  function handleChange() {
+    setInStockOnly((prev) => !prev);
+  }
+
+  return (
+    <main className="container mx-auto w-1/2 flex flex-col items-center mt-8">
+      <Search searchHandler={handleKeyUp} inStockHandler={handleChange} />
+      <Table />
+    </main>
+  );
+};
 
 export default FilterableProductTable;
