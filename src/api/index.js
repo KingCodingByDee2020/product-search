513c565fc8592957f507d130b6f2847b69e91df4
Manav Misra <manavm@visionwebsoft.com>
2021-02-25 17:50:23 -0600

    :recycle: Replace fetch with ky

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,10 @@
+import ky from "ky";
+
 const api = {
   async index(
     endpoint = "http://my-json-server.typicode.com/claim-academy-js/products/products"
   ) {
-    // res is a RESPONSE STREAM
-    const res = await fetch(endpoint);
-
-    // Read the STREAM BODY as JSON
-    return res.json();
+    return ky.get(endpoint).json();
   },
 };
 
